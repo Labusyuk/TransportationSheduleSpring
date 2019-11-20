1570bc6ef2a5596e3d229f9e146f0f57b1d6033b
Vitaliy Labusyuk <labusyuk@gmail.com>
2019-11-20 11:07:56 +0200

    added MyConfig.java

---
+++ src/main/java/com/labus/transportation/config/MyConfig.java
@@ -0,0 +1,9 @@
+package com.labus.transportation.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.labus.transportation")
+public class MyConfig {
+}
